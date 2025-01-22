36daa615a2bf540bd3f392c0b2c064c9f30c9fe3
satoru <rhodeus208@gmail.com>
2025-01-22 13:22:17 +0900

    キャンセル分析

--- daily-practice/20250122-1/query.sql
+++ daily-practice/20250122-1/query.sql
@@ -20,3 +20,29 @@ select
         from dept_stats ds
             order by
                 ds.appointment_within_30days desc;
+
+
+
+2024年1月に3回以上予約をキャンセル（status = 'cancelled'）した患者の一覧を、キャンセル回数と共に表示してください。
+with cancel_stats as (
+    select
+        pat.*,
+        count(app.appointment_id) as cancelled_app
+            from
+                patients pat
+                    inner join appointments app on app.patient_id = pat.patient_id
+            where
+                date_format(app.appointment_date,'%Y-%m')='2024-01'
+                    and
+                app.status = 'cancelled'
+            group by
+                pat.patient_id,
+                pat.name,
+                pat.birth_date,
+                pat.blood_type
+)
+select
+    cs.*
+       from cancel_stats cs 
+            where cs.cancelled_app >= 3;
+            
