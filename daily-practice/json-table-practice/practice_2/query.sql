97d28d5af676c067db8e198b531ed20a0b42c4de
satoru <rhodeus208@gmail.com>
2025-02-23 21:20:24 +0900

    JSON_TABLEを利用する際になぜかLEFT JOINが使えない特殊な制約がMySQLにはあるようだ

--- daily-practice/json-table-practice/practice_2/query.sql
+++ daily-practice/json-table-practice/practice_2/query.sql
@@ -18,3 +18,41 @@ FROM
 |佐藤医師|外科       |
 |佐藤医師|整形外科     |
 
+SELECT
+	doc.name,
+	lang.primary_language,
+	flang.foreign_language
+FROM
+	doctors doc
+	INNER JOIN JSON_TABLE(
+		doc.profile->'$.languages','$'
+		COLUMNS(
+			primary_language VARCHAR(255) PATH '$.primary'
+		)
+	) AS lang
+	INNER JOIN JSON_TABLE(
+		doc.profile->'$.languages.foreign','$[*]'
+		COLUMNS(
+			foreign_language VARCHAR(255) PATH '$'
+		)
+	) AS flang;
+
+|name|primary_language|foreign_language|
+|----|----------------|----------------|
+|山田医師|日本語             |英語              |
+|山田医師|日本語             |中国語             |
+|鈴木医師|日本語             |英語              |
+
+２つ目のINNERをLEFTに変えると
+
+Error occurred during SQL query execution
+
+Reason:
+SQL Error [1064] [42000]: You have an error in your SQL syntax; check the manual that corresponds to your MySQL server version for the right syntax to use near '' at line 18
+
+このエラーがでる。難しい。このMySQLのJSON_TABLEの特殊な制約はよくわかんない。
+
+
+
+
+
